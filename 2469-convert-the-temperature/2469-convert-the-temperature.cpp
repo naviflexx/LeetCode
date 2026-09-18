386bf63261db81e9fc1c1d30b98fba98937ec5d6
Navi <connect.naavi@gmail.com>
2026-09-18 12:41:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (48.39%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    vector<double> convertTemperature(double celsius) 
+    {
+        double Kelvin , Fahrenheit;
+        Kelvin = celsius + 273.15;
+        Fahrenheit = (celsius*1.80) + 32.00;
+        vector<double> ans(2);
+        ans[0]= Kelvin;
+        ans[1]= Fahrenheit;
+
+        return ans;
+
+
+    }
+};
