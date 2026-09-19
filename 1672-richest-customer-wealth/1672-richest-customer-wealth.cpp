75ff779fb07040b6ba4079317bb0ea4bb8bbfca2
Navi <connect.naavi@gmail.com>
2026-09-19 18:11:47 +0530

    Time: 0 ms (100%), Space: 11.4 MB (54.94%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    int maximumWealth(vector<vector<int>>& accounts) 
+    {
+        int sum=0;
+        for(int i=0; i<accounts.size(); i++)
+        {
+            int ct=0;
+            for(int j=0; j<accounts[i].size(); j++)
+            {
+                ct+= accounts[i][j];
+            }
+            if(ct>sum)
+            {
+            sum=ct;
+            }
+        }
+        return sum;
+    }
+};
